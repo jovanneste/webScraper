d8279b7e77b73e75438bba6ca04d96e8efe0c5d7
AwsomeChaps <93431590+AwsomeChaps@users.noreply.github.com>
2021-10-30 23:43:14 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 
 goodbad = {'zero':'loads of', "'unjustified'":'very justified', 'failure':'success', 'mass':'no', 
 			'shortages':'surpluss', 'homeless':'millionaies', 'overlooked':'helped', 'kills':'saves', 
-			'kill':'marry', 'warn':'remind', 'slow-motion':'really fast paced', 'deaths;':'puppies'}
+			'kill':'marry', 'warn':'remind', 'slow-motion':'really fast paced', 'deaths;':'puppies', 'ugly':'beatiful',
+			'murder':'hug', 'murdered':'hugged', 'bad':'good', 'sad':'happy', 'sorry':'extatic', 'drowning':'flying', 
+			'crash':'party', 'killed':'married', 'concerning':'exciting', 'killing':'saving', 'warns':'announces',
+			'hospital':'theme park', 'knifepoint':'a cake', 'gunpoint':'a cake', 'covid':''}
 
 def main():
 
@@ -11,16 +14,16 @@ def main():
 	newheadlines = []
 
 	url='https://www.theguardian.com/uk'
+	
 
 	response = requests.get(url)
 
 	soup = BeautifulSoup(response.text, 'html.parser')
 	heads = soup.find('body').find_all('h3')
 	for x in heads:
-		headlines.append(x.text.strip())
-		headlines = headlines[0:30]
+		headlines.append(x.text.strip().lower())
+		headlines = headlines[0:100]
 
-	
 	for j in headlines:
 		if (any(x in j for x in goodbad.keys())):
 			h = j.split(' ')
