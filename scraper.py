e7d867d206e13329450002a1a140d087350f454c
Joachim Vanneste <2460800V@student.gla.ac.uk>
2021-10-30 21:49:40 +0100

    text replacement added

--- scraper.py
+++ scraper.py
@@ -1,16 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
+goodbad = {'zero':'loads of', "'unjustified'":'very justified', 'failure':'success', 'mass':'no', 
+			'shortages':'surpluss', 'homeless':'millionaies', 'overlooked':'helped', 'kills':'saves', 
+			'kill':'marry', 'warn':'remind', 'slow-motion':'really fast paced', 'deaths;':'puppies'}
+
 def main():
 
+	headlines = []
+	newheadlines = []
 
 	url='https://www.theguardian.com/uk'
+
 	response = requests.get(url)
 
 	soup = BeautifulSoup(response.text, 'html.parser')
-	headlines = soup.find('body').find_all('h3')
-	for x in headlines:
-	    print(x.text.strip())
+	heads = soup.find('body').find_all('h3')
+	for x in heads:
+		headlines.append(x.text.strip())
+		headlines = headlines[0:30]
+
+	
+	for j in headlines:
+		if (any(x in j for x in goodbad.keys())):
+			h = j.split(' ')
+			replacer = goodbad.get
+			newsentence = [replacer(n,n) for n in h]
+			newheadlines.append(' '.join(newsentence))
+
+
+	#only show nicer headlines
+	for h in newheadlines:
+		print(h)
+	
+
+
+
 
 
 if __name__ == '__main__':
